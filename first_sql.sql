bb7f0c4ceae7b832e129092cdecdbc98badd5c6c
Francois Basson <fafprogrammer@gmail.com>
2023-05-26 16:10:21 +0200

    Update first_sql.sql

--- first_sql.sql
+++ first_sql.sql
@@ -7,3 +7,19 @@ sex VARCHAR(1),
 salary INT,
 super_id INT,
 branch_id INT);
+
+CREATE TABLE branch(
+branch_id INT PRIMARY KEY,
+branch_name VARCHAR(40),
+mgr_id INT,
+mgr_start_date DATE,
+FOREIGN KEY(mgr_id) REFERENCES employee(emp_id) ON DELETE SET NULL);
+
+ALTER TABLE employee
+DROP COLUMN birth_day; 
+
+ALTER TABLE employee
+ADD COLUMN birth_day DATE;
+
+
+
