aebde485112b3726f5f1b83dc6bcc234f5111b2d
Francois Basson <fafprogrammer@gmail.com>
2023-05-29 17:40:30 +0200

    Update first_sql.sql

--- first_sql.sql
+++ first_sql.sql
@@ -104,7 +104,7 @@ SET foreign_key_checks = 1;
 
 --INSERTING DATA INTO TABLES
 
---CORPORATE BRANCH
+--CORPORATE BRANCH ENTER INFO
 INSERT INTO employee VALUES('100', 'David', 'Wallace', '1963/11/17','M', '250000', NULL, NULL);
 INSERT INTO branch VALUES(1, 'Corporate', 100, '2006/02/09');
 
@@ -114,7 +114,7 @@ WHERE emp_id =100;
 
 INSERT INTO employee VALUES(101, 'Jan', 'Levinston', '1961/05/11', 'F', '110000', 100, 1);
 
---SCRANTON BRANCH
+--SCRANTON BRANCH ENTER INFO
 
 INSERT INTO employee VALUES(102, 'Micheal', 'Scott', '1964/03/15', 'M', '75000', 100, NULL);
 INSERT INTO branch VALUES(2, 'Scranton', 100, '1992/04/06');
@@ -127,7 +127,7 @@ INSERT INTO employee VALUES(103, 'Angela', 'Martin', '1971/06/25', 'M', '63000',
 INSERT INTO employee VALUES(104, 'Kelly', 'Kapoor', '1980/02/05', 'F', '55000', 102, 2);
 INSERT INTO employee VALUES(105, 'Stanley', 'Hudson', '1958/02/19', 'F', '69000', 102, 2);
 
---STAMFORD
+--STAMFORD ENTER INFO
 
 INSERT INTO employee VALUES(106, 'Josh', 'Porter', '1989/09/06', 'M', '78000', 100, NULL);
 
@@ -139,7 +139,35 @@ INSERT INTO employee VALUES(107, 'Andy', 'Bernard', '1973/07/22', 'M', '65000',1
 INSERT INTO employee VALUES(108, 'Jen', 'Helpoot', '1978/10/01', 'M', '71000', 106, 3);
 INSERT INTO branch VALUES(3, 'Stamford', 106, '1998/02/13');
 
+--CLIENT ENTER INFO 
+INSERT INTO client VALUES(400, 'Dunmore Highschool', 2);
+INSERT INTO client VALUES(401, 'Lackwana Country', 2);
+INSERT INTO client VALUES(402, 'FedEx', 3);
+INSERT INTO client VALUES(403, 'John Daly Law, LLC', 3);
+INSERT INTO client VALUES(404, 'Scranton Whitepages', 2);
+INSERT INTO client VALUES(405, 'Times Newspaper', 3);
+INSERT INTO client VALUES(406, 'FedEx', 2);
 
+--BRANCH SUPPLIER ENTER INFO
+INSERT INTO branch_supplier VALUES(2, 'Hammermill', 'Paper');
+INSERT INTO branch_supplier VALUES(2, 'JT Forms and Labels', 'Custom Forms');
+INSERT INTO branch_supplier VALUES(2, 'Uniball', 'Writing Utensils');
+INSERT INTO branch_supplier VALUES(3, 'Hammermill', 'Paper');
+INSERT INTO branch_supplier VALUES(3, 'Patriot Paper', 'Paper');
+INSERT INTO branch_supplier VALUES(3, 'Stamford Labels', 'Custom forms');
+INSERT INTO branch_supplier VALUES(3, 'Uniball', 'Writing Utensils');
+
+--WORKS_WITH ENTER INFO
+
+INSERT INTO works_with VALUES(102, 401, 267000);
+INSERT INTO works_with VALUES(102, 406, 15000);
+INSERT INTO works_with VALUES(105, 400, 55000);
+INSERT INTO works_with VALUES(105, 404, 33000);
+INSERT INTO works_with VALUES(105, 406, 130000);
+INSERT INTO works_with VALUES(107, 403, 5000);
+INSERT INTO works_with VALUES(107, 405, 26000);
+INSERT INTO works_with VALUES(108, 402, 22500);
+INSERT INTO works_with VALUES(108, 403, 12000);
 
 SELECT * FROM employee;
 SELECT * FROM branch;
